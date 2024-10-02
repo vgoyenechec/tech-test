f206032f85e8f4d059d612887453cf053a069a19
Valentina <valegoye@gmail.com>
2024-10-02 12:00:49 -0500

    initial commit

---
+++ src/main/java/bgt/tech_test/TechTestApplication.java
@@ -0,0 +1,13 @@
+package bgt.tech_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bgt/tech_test/TechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package bgt.tech_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
